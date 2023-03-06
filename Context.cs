4eb8142089fcf046fbdb5ea80c66afc493904c40
DnTo <danto@hotmail.com>
2023-03-06 18:45:08 -0500

    Add project files.

---
+++ Context.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mysql_EF
+{
+    public class Context:DbContext
+    {
+        public DbSet<Model> Models { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=db4free.net;database=efmysql;user=efmysql;password=Efmysql123!");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Model>(entity =>
+            //{
+            //    entity.HasKey(e => e.Id);
+            //    entity.Property(e => e.Name).IsRequired();
+            //});
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+/**
+ Install-Package MySql.EntityFrameworkCore -Version 6.0.4
+ 
+ 
+ 
+ */
+
+
+// See https://aka.ms/new-console-template for more information
+using Mysql_EF;
+
+var context = new Context();
+Console.WriteLine("Hello MySql!");
+Console.WriteLine("Creating Database");
+context.Database.EnsureCreated();
+Console.WriteLine("Database Created");
+var model = new Model() { Name = "EF" };
+context.Models.Add(model);
+context.SaveChanges();
+Console.WriteLine( $"Name = {model.Name} Id = {model.Id}");
+
+Console.ReadLine();
